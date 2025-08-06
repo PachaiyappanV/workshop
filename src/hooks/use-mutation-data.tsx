d1bd829a90f323ada78b421c4dbfc87d973331db
PachaiyappanV <pachaiyappan394@gmail.com>
2025-08-06 12:53:05 +0530

    feat(hook): add mutation-data hook

---
+++ src/hooks/use-mutation-data.tsx
@@ -0,0 +1,46 @@
+import {
+  MutationFunction,
+  MutationKey,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { toast } from "sonner";
+
+export const useMutationData = (
+  mutationKey: MutationKey,
+  mutationFn: MutationFunction<any, any>,
+  onSuccess?: () => void,
+  queryKeys?: string[][]
+) => {
+  const client = useQueryClient();
+  const { mutate, isPending } = useMutation({
+    mutationKey,
+    mutationFn,
+    onSuccess(data) {
+      if (onSuccess) onSuccess();
+
+      if (data?.status == 200 || data?.status == 201) {
+        toast.success(data?.message);
+      } else {
+        toast.error(data?.message);
+      }
+    },
+    onSettled: async () => {
+      if (queryKeys && queryKeys[0]) {
+        await client.invalidateQueries({
+          queryKey: queryKeys[0],
+          exact: true,
+        });
+      }
+
+      if (queryKeys && queryKeys[1]) {
+        await client.invalidateQueries({
+          queryKey: queryKeys[1],
+          exact: true,
+        });
+      }
+    },
+  });
+
+  return { mutate, isPending };
+};
